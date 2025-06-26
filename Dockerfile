0d4e2a07690bb729e0bf060a8ac5e09d34466ee4
Isheunesu Tembo <isheunesu48@gmail.com>
2025-06-26 15:39:58 +0300

    removed sigmoid from the last layer

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.7.3-stretch
+FROM python:3.9-slim
 
 # Maintainer info
 LABEL maintainer="isheunesutembo@gmail.com"
--- main.py
+++ main.py
@@ -1,39 +1,47 @@
-from fastapi import FastAPI
+
+from fastapi import FastAPI, File, UploadFile
+from keras.src.applications.convnext import preprocess_input
 from tensorflow.keras.models import load_model
 from tensorflow.keras.utils import get_file
 from tensorflow.keras.utils import load_img
 from tensorflow.keras.utils import img_to_array
 from tensorflow import expand_dims
 from tensorflow.nn import softmax
+from tensorflow.nn import sigmoid
 from numpy import argmax
 from numpy import max
 from numpy import array
 from json import dumps
 from uvicorn import run
 import os
+import uvicorn
+from io import BytesIO
+from PIL import Image
+
 
 app=FastAPI()
 
-model_dir=""
+model_dir="pneumonia_model.h5"
 model=load_model(model_dir)
 
 
 @app.post("/predict")
-async def pneumonia_prediction(image_link:str=""):
-    if image_link == "":
-        return {"message":"Please provide Image Link"}
+async def pneumonia_prediction(file: UploadFile = File(...)):
+    image_data = await file.read()
+    image = Image.open(BytesIO(image_data)).convert("RGB")
+    image = image.resize((224, 224))
 
-    img_path=get_file(origin=image_link)
-    img=load_img(img_path,target_size=(224,224))
-    img_array=img_to_array(img)
+
+    classes = ['NORMAL', 'PNEUMONIA']
+
+    img_array=img_to_array(image)
+    img_array = img_array / 255.0
     img_array=expand_dims(img_array,axis=0)
     pred=model.predict(img_array)
 
-    score=softmax(pred[0])
-
-    class_prediction=class_predictions[argmax(score)]
-
-    model_score=round(max(score)*100,2)
+    score = float(pred[0][0])  # single float output
+    class_prediction = "PNEUMONIA" if score > 0.5 else "NORMAL"
+    model_score = round(float(score if score > 0.5 else 1 - score) * 100, 2)
 
     return {
         "prediction":class_prediction,
@@ -42,5 +50,5 @@ async def pneumonia_prediction(image_link:str=""):
 
 
 if __name__=="__main__":
-    port=int(os.environ.get("PORT",5000))
-    run(host="0.0.0.0",port=port)
+    port=int(os.environ.get("PORT",3000))
+    uvicorn.run(app, host="127.0.0.1", port=port)
